b037003e513a19e9606c152f3cc492fe73b9fa8b
Arik Hativa <arikhativa@gmail.com>
2022-11-15 11:33:50 +0200

    added to scripts

---
+++ scripts/create_c_main.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+REPO=linux-settings
+RES=res
+FILE=main.c
+
+cp $HOME/$REPO/$RES/$FILE .
---
+++ scripts/create_cpp_main.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+REPO=linux-settings
+RES=res
+FILE=main.cpp
+
+cp $HOME/$REPO/$RES/$FILE .
