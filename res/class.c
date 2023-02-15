670c5a870eddb20db27ab464ea66c01d2f0a2a4e
Arik Hativa <arikhativa@gmail.com>
2023-02-15 13:56:03 +0200

    asd

--- res/class.c
+++ res/class.c
@@ -6,7 +6,7 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 14:00:59 by yrabby            #+#    #+#             */
-/*   Updated: 2023/02/15 13:37:33 by yrabby           ###   ########.fr       */
+/*   Updated: 2023/02/15 13:55:57 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,26 @@
 
 t_error_code	Replace_create(t_Replace **ret)
 {
-	if (ret)
-	{
-		*ret = (t_Replace *)ft_calloc(1, sizeof(t_Replace));
-		if (!*ret)
-			return (ERROR);
-		return (SUCCESS);
-	}
-	return (ERROR);
+	t_error_code	err;
+	t_Replace		*tmp;
+
+	if (!ret)
+		return (ERROR);
+	tmp = (t_Replace *)ft_calloc(1, sizeof(t_Replace));
+	if (!tmp)
+		return (ALLOCATION_ERROR);
+	*ret = tmp;
+	return (SUCCESS);
 }
 
 void	Replace_destroy(t_Replace **obj)
 {
 	t_Replace	*tmp;
 
-	if (obj && *obj)
-	{
-		tmp = *obj;
-		ft_bzero(tmp, sizeof(t_Replace));
-		free(tmp);
-		*obj = NULL;
-	}
+	if (!obj && !*obj)
+		return ;
+	tmp = *obj;
+	ft_bzero(tmp, sizeof(t_Replace));
+	free(tmp);
+	*obj = NULL;
 }
